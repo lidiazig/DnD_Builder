c61f57310dc8cf3bcacf621cf0949babdcd22379
Lidia <lidiazig@hotmail.es>
2020-03-12 13:16:19 +0100

    Interfaz login, lista y pj Fragments creados Fragments perfil y caracteristicas hechos

--- app/src/main/java/com/lidia/ddbuilder/PersonajeActivity.java
+++ app/src/main/java/com/lidia/ddbuilder/PersonajeActivity.java
@@ -43,15 +43,5 @@ public class PersonajeActivity extends AppCompatActivity {
                     break;
             }
         }
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 }
