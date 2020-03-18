547287ead893b975715ab404f89d109da5f5c259
Lidia <lidiazig@hotmail.es>
2020-03-18 12:00:22 +0100

    Interfaz login, lista y pj Fragments creados Fragments perfil y caracteristicas hechos Layouts traducidos al inglés

--- app/src/main/java/com/lidia/ddbuilder/PersonajeActivity.java
+++ app/src/main/java/com/lidia/ddbuilder/PersonajeActivity.java
@@ -2,28 +2,31 @@ package com.lidia.ddbuilder;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
+import android.widget.Button;
 
 import com.lidia.ddbuilder.ui.fragments.SectionsPagerAdapter;
 
 public class PersonajeActivity extends AppCompatActivity {
 
+    private Button btnHome, btnSave;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personaje);
+        btnHome = findViewById(R.id.btnHome);
+        btnSave = findViewById(R.id.btnSave);
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabLayout = findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
+
 
         for (int i = 0; i < tabLayout.getTabCount(); i++) {
             switch (i) {
